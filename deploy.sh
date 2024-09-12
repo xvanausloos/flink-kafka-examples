7b8af1bd7495433c21d4ff5783995cb267871a4b
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-09-12 12:47:57 +0200

    deployment script ok

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-cp ./target/flink-kafka-examples-1.0.jar ~/wordcountflinkkafka.jar
+target=~/projects/flink-kafka-docker-compose/wordcountflinkkafka.jar
+cp ./target/flink-kafka-examples-1.0.jar $target
+
+if [[ $? -eq 0 ]]; then
+    echo "Script succeeded"
+else
+    echo "Script failed"
+fi
+
